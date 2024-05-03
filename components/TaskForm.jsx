53b0d0552581cf61a37f90813bc22ca13fa74b09
Taofeek Adeniyi <105851811+tee4tao@users.noreply.github.com>
2024-05-03 18:48:04 +0100

    learning server actions

---
+++ components/TaskForm.jsx
@@ -0,0 +1,36 @@
+import prisma from "@/utils/db";
+import { revalidatePath } from "next/cache";
+
+const createTask = async (formData) => {
+  "use server";
+  const content = formData.get("content");
+  // some validation here
+
+  await prisma.task.create({
+    data: {
+      content,
+    },
+  });
+  // revalidate path
+  revalidatePath("/tasks");
+};
+
+const TaskForm = () => {
+  return (
+    <form action={createTask}>
+      <div className="join w-full">
+        <input
+          className="input input-bordered join-item w-full"
+          placeholder="Type Here"
+          type="text"
+          name="content" // Important! Anytime we're working with server actions, if we wanna access thr input value, we have to set up a name
+          required //so user won't submit the form with empty values
+        />
+        <button type="submit" className="btn join-item btn-primary">
+          create task
+        </button>
+      </div>
+    </form>
+  );
+};
+export default TaskForm;
